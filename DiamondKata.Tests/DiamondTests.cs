ece570954a57376c1692d0daf0ae20983b9a1240
Adam Friswell <adamfriswell@googlemail.com>
2023-01-06 15:43:32 +0000

    break tests by expecting bottom half of triangle

--- DiamondKata.Tests/DiamondTests.cs
+++ DiamondKata.Tests/DiamondTests.cs
@@ -9,15 +9,15 @@ namespace DiamondKata.Tests
         }
 
         [Fact]
-        public void B_should_repeat_characters_with_linebreak()
+        public void B_should_give_full_diamond_letters()
         {
-            Assert.Equal("A\nBB\n", Diamond.Create('B'));
+            Assert.Equal("A\nBB\nA\n", Diamond.Create('B'));
         }
 
         [Fact]
-        public void C_should_repeat_characters_with_linebreak()
+        public void C_should_should_give_full_diamond_letters()
         {
-            Assert.Equal("A\nBB\nCCC\n", Diamond.Create('C'));
+            Assert.Equal("A\nBB\nCCC\nBB\nA\n", Diamond.Create('C'));
         }
     }
 }
