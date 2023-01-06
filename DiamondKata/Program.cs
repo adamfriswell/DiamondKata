dca28c8c30952fa6c5e13395b63d38ce256bd1c4
Adam Friswell <adamfriswell@googlemail.com>
2023-01-06 16:03:58 +0000

    write main method to take letter input so can see visualisation of diamond

--- DiamondKata/Program.cs
+++ DiamondKata/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DiamondKata;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool exit = false;
+        while (!exit)
+        {
+            Console.Write("Enter a letter (SPACE to exit): ");
+            var input = Console.ReadLine().ToUpper()[0];
+            if(input == ' ')
+            {
+                exit = true;
+            }
+            else
+            {
+                var result = Diamond.Create(input);
+                Console.WriteLine(result);
+                Console.WriteLine("------------");
+            }
+        }
+    }
+}
