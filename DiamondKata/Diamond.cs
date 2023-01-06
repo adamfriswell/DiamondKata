83fd6ef7526040452e0cd5838e447688c5f300ce
Adam Friswell <adamfriswell@googlemail.com>
2023-01-06 15:31:58 +0000

    passing test by finding position of letter in alphabet

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -2,9 +2,21 @@
 {
     public static class Diamond
     {
+        public static List<char> Letters() => new List<char>() {'A', 'B', 'C', 'D', 'E', 'F',
+                     'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                     'U', 'V', 'W', 'X', 'Y', 'Z' };
+
         public static string Create(char input)
         {
-            return input.ToString();
+            var alphabet = Letters();
+            var alphabetIndex = alphabet.IndexOf(input) + 1;
+            var subsetLetters = alphabet.Take(alphabetIndex).ToList();
+            var result = "";
+            foreach(var l in subsetLetters)
+            {
+                result += l.ToString();
+            }
+            return result;
         }
     }
 }
