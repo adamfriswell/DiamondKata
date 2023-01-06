3445213f8e4f37071f07a3115c6b93610c7e5b84
Adam Friswell <adamfriswell@googlemail.com>
2023-01-06 15:51:47 +0000

    refactor Create method in Diamond class into methods

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -10,20 +10,32 @@
         {
             var alphabet = Letters();
             var inputIndex = alphabet.IndexOf(input) + 1;
-            var subset = alphabet.Take(inputIndex).ToList();
-            var result = "";
-            foreach(var letter in subset)
-            {
-                var letterIndex = alphabet.IndexOf(letter) + 1;
-                for (int i = 0; i < letterIndex; i++)
-                {
-                    result += letter.ToString();
-                }
-                result += "\n";
-            }
-            var subsetWithoutInput = alphabet.Take(inputIndex-1).ToList();
+
+            string topOfDiamond = CreateTopOfDiamond(alphabet, inputIndex);
+            string bottomOfDiamond = CreateBottomOfDiamond(alphabet, inputIndex);
+
+            return topOfDiamond + bottomOfDiamond;
+        }
+
+        private static string CreateBottomOfDiamond(List<char> alphabet, int inputIndex)
+        {
+            var subsetWithoutInput = alphabet.Take(inputIndex - 1).ToList();
             subsetWithoutInput.Reverse();
-            foreach (var letter in subsetWithoutInput)
+            var bottomOfDiamond = CreateTriangleFromSubset(alphabet, subsetWithoutInput);
+            return bottomOfDiamond;
+        }
+
+        private static string CreateTopOfDiamond(List<char> alphabet, int inputIndex)
+        {
+            var subset = alphabet.Take(inputIndex).ToList();
+            var topOfDiamond = CreateTriangleFromSubset(alphabet, subset);
+            return topOfDiamond;
+        }
+
+        private static string CreateTriangleFromSubset(List<char> alphabet, List<char> subset)
+        {
+            var result = "";
+            foreach (var letter in subset)
             {
                 var letterIndex = alphabet.IndexOf(letter) + 1;
                 for (int i = 0; i < letterIndex; i++)
@@ -32,6 +44,7 @@
                 }
                 result += "\n";
             }
+
             return result;
         }
     }
