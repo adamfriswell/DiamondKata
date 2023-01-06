ff7c1f10414469f5ecb22720a09516e72425900f
Adam Friswell <adamfriswell@googlemail.com>
2023-01-06 15:39:23 +0000

    break tests by requiring new line character after each letter

--- DiamondKata.Tests/DiamondTests.cs
+++ DiamondKata.Tests/DiamondTests.cs
@@ -5,19 +5,19 @@ namespace DiamondKata.Tests
         [Fact]
         public void A_should_give_singelton_diamond()
         {
-            Assert.Equal("A", Diamond.Create('A'));
+            Assert.Equal("A\n", Diamond.Create('A'));
         }
 
         [Fact]
         public void B_should_repeat_characters()
         {
-            Assert.Equal("ABB", Diamond.Create('B'));
+            Assert.Equal("A\nBB\n", Diamond.Create('B'));
         }
 
         [Fact]
         public void C_should_repeat_characters()
         {
-            Assert.Equal("ABBCCC", Diamond.Create('C'));
+            Assert.Equal("A\nBB\nCCC\n", Diamond.Create('C'));
         }
     }
 }
