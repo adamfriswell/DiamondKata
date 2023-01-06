9734a702efe50360dd29514dd6fc19b4f80f26ab
Adam Friswell <adamfriswell@googlemail.com>
2023-01-06 15:14:41 +0000

    failing test for character sequence for B

--- DiamondKata.Tests/DiamondTests.cs
+++ DiamondKata.Tests/DiamondTests.cs
@@ -1,11 +1,17 @@
 namespace DiamondKata.Tests
 {
-    public class DiamonTests
+    public class DiamondTests
     {
         [Fact]
         public void A_should_give_singelton_diamond()
         {
             Assert.Equal("A", Diamond.Create('A'));
+        }
+
+        [Fact]
+        public void B_should_give_character_sequence()
+        {
+            Assert.Equal("AB", Diamond.Create('B'));
         }
     }
 }
