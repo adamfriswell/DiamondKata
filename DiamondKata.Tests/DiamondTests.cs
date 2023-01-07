09ed0e2465bdadad6ba9bf038d4bf91a18f8ec4f
Adam Friswell <adamfriswell@googlemail.com>
2023-01-07 17:29:53 +0000

    failing unit tests to give left padding of each line of diamond

--- DiamondKata.Tests/DiamondTests.cs
+++ DiamondKata.Tests/DiamondTests.cs
@@ -11,13 +11,13 @@ namespace DiamondKata.Tests
         [Fact]
         public void B_should_give_full_diamond_with_underscores_for_middle_gap()
         {
-            Assert.Equal("A\nB_B\nA\n", Diamond.Create('B'));
+            Assert.Equal("_A\nB_B\n_A\n", Diamond.Create('B'));
         }
 
         [Fact]
         public void C_should_give_full_diamond_with_underscores_for_middle_gap()
         {
-            Assert.Equal("A\nB_B\nC___C\nB_B\nA\n", Diamond.Create('C'));
+            Assert.Equal("__A\n_B_B\nC___C\n_B_B\n__A\n", Diamond.Create('C'));
         }
     }
 }
